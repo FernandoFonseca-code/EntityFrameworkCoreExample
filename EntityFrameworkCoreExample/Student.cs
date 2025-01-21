a2a108e2a28cc1a66bbde9f372e3e00b9a442520
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-01-21 15:42:37 -0800

    Created Student model to track in the database

---
+++ EntityFrameworkCoreExample/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreExample
+{
+    public class Student
+    {
+        [Key] // Tell EF Core to make this a primary key (PK)
+        /// <summary>
+        /// T
+        /// </summary>
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -9,7 +9,19 @@ namespace EntityFrameworkCoreExample
 {
     // EF Core Getting Started
     // https://learn.microsoft.com/en-us/ef/core/get-started/overview/install
-    internal class StudentContext : DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Database = The desired name for the database
+            // Server = The server we are connecting to. LocalDB is included with Visual Studio
+            // Trusted_Connection = True means we are using Windows Authentication
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCoreExample;Trusted_connection=True;");
+        }
     }
 }
